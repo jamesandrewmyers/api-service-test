9b87f8dec0805356433ae3a6671ce8a578e0dc91
James Andrew Myers <jamesandrewmyers1@gmail.com>
2025-08-19 02:19:04 -0700

    match stack name to deployment workflow configuration (#36)
    Co-authored-by: James Andrew Myers <jmyers@Jamess-MacBook-Pro.local>

--- cdk/app.py
+++ cdk/app.py
@@ -3,5 +3,5 @@ import aws_cdk as cdk
 from lambda_stack import LambdaStack   # adjust to your file/class
 
 app = cdk.App()
-LambdaStack(app, "ApiServiceTestStack", description="API Service Test Stack")
+LambdaStack(app, "api-service-test-dev", description="API Service Test Stack")
 app.synth()
