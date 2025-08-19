dde157cd4a325e41a751c7dbcb4678e33cb65639
James Andrew Myers <jamesandrewmyers1@gmail.com>
2025-08-18 21:00:01 -0700

    add description to CDK stack (#31)
    Co-authored-by: James Andrew Myers <jmyers@Jamess-MacBook-Pro.local>

--- cdk/app.py
+++ cdk/app.py
@@ -3,5 +3,5 @@ import aws_cdk as cdk
 from lambda_stack import LambdaStack   # adjust to your file/class
 
 app = cdk.App()
-LambdaStack(app, "ApiServiceTestStack")
+LambdaStack(app, "ApiServiceTestStack", description="API Service Test Stack")
 app.synth()
