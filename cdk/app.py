e80b49839e09caa8d9c97bfd6698fbc8619e5281
James Andrew Myers <jmyers@Jamess-MacBook-Pro.local>
2025-08-18 19:33:49 -0700

    update CDK stack definition with Lambda function

--- cdk/app.py
+++ cdk/app.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python3
-import aws_cdk as cdk
-from lambda_stack import LambdaStack
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+)
+from constructs import Construct
 
-app = cdk.App()
-LambdaStack(app, "ApiServiceTestStack")
-app.synth()
+class ApiServiceTestStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        _lambda.Function(
+            self, "ApiServiceTestLambda",
+            runtime=_lambda.Runtime.PYTHON_3_11,
+            handler="lambda_function.lambda_handler",
+            code=_lambda.Code.from_asset("./artifact"),  # or "./" if code is in repo root
+        )
