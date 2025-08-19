c4726bcd10aead011f90830d989b9489545cdb95
James Andrew Myers <jmyers@Jamess-MacBook-Pro.local>
2025-08-18 20:07:57 -0700

    fix CDK app imports and structure

--- cdk/app.py
+++ cdk/app.py
@@ -1,16 +1,7 @@
-from aws_cdk import (
-    Stack,
-    aws_lambda as _lambda,
-)
-from constructs import Construct
+#!/usr/bin/env python3
+import aws_cdk as cdk
+from cdk.lambda_stack import LambdaStack   # adjust to your file/class
 
-class ApiServiceTestStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs):
-        super().__init__(scope, construct_id, **kwargs)
-
-        _lambda.Function(
-            self, "ApiServiceTestLambda",
-            runtime=_lambda.Runtime.PYTHON_3_11,
-            handler="lambda_function.lambda_handler",
-            code=_lambda.Code.from_asset("./artifact"),  # or "./" if code is in repo root
-        )
+app = cdk.App()
+LambdaStack(app, "ApiServiceTestStack")
+app.synth()
