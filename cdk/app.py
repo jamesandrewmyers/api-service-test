8f02854efc1bd3564c50d896f8793b54ed858f22
James Andrew Myers <jamesandrewmyers1@gmail.com>
2025-08-18 20:33:07 -0700

    fix lambda stack import path (#30)
    Co-authored-by: James Andrew Myers <jmyers@Jamess-MacBook-Pro.local>

--- cdk/app.py
+++ cdk/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from cdk.lambda_stack import LambdaStack   # adjust to your file/class
+from lambda_stack import LambdaStack   # adjust to your file/class
 
 app = cdk.App()
 LambdaStack(app, "ApiServiceTestStack")
